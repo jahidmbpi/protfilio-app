3f3df325f1b36c94349606032cde19c074206b32
Jahidul Islam <jahid.redmi10@gmail.com>
2025-10-22 12:49:56 +0600

    update

--- src/component/Home/Banner.tsx
+++ src/component/Home/Banner.tsx
@@ -6,15 +6,15 @@ import animationData from "../../../public/banner.json";
 export default function Banner() {
   return (
     <div className="max-w-7xl mx-auto w-full ">
-      <div className=" items-center justify-between flex h-screen">
-        <div className="flex-1 space-y-4">
+      <div className=" items-center justify-between flex h-[120vh] flex-col md:flex-row">
+        <div className="flex-1 space-y-5">
           <h2 className="capitalize text-[16px]  font-sans p-[4px]">hi,I’m</h2>
 
           <h2 className="text-4xl font-semibold font-sans">
             <Highlight> jahidul islam</Highlight>
           </h2>
 
-          <h2 className="text-6xl font-bold text-[#112A46] uppercase">
+          <h2 className="text-6xl font-medium font-popinse text-[#112A46] uppercase">
             <Typewriter
               words={[
                 "Passionate MERN ",
@@ -29,7 +29,7 @@ export default function Banner() {
               cursorStyle="|"
             />
           </h2>
-          <p className="font-sans">
+          <p className="font-sans text-[#5b5454]">
             Hi, I’m Jahid — a passionate MERN Stack Developer dedicated to
             building creative, functional, and performance-driven web solutions.
           </p>
