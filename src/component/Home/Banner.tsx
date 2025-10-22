0b799a9aef2e36bccf5d7643740f2d75bcb96f3f
Jahidul Islam <jahid.redmi10@gmail.com>
2025-10-22 12:39:07 +0600

    impliment lottiefile svg animation

--- src/component/Home/Banner.tsx
+++ src/component/Home/Banner.tsx
@@ -1,24 +1,25 @@
 "use client";
 import { Highlight } from "@/components/ui/hero-highlight";
+import Lottie from "lottie-react";
 import { Typewriter } from "react-simple-typewriter";
-
+import animationData from "../../../public/banner.json";
 export default function Banner() {
   return (
     <div className="max-w-7xl mx-auto w-full ">
       <div className=" items-center justify-between flex h-screen">
         <div className="flex-1 space-y-4">
-          <h2 className="capitalize text-[20px]  font-sans">hi,i am</h2>
+          <h2 className="capitalize text-[16px]  font-sans p-[4px]">hi,I’m</h2>
 
-          <h2 className="text-4xl  font-semibold font-sans">
+          <h2 className="text-4xl font-semibold font-sans">
             <Highlight> jahidul islam</Highlight>
           </h2>
 
-          <h2 className="text-6xl font-bold text-black uppercase">
+          <h2 className="text-6xl font-bold text-[#112A46] uppercase">
             <Typewriter
               words={[
                 "Passionate MERN ",
                 "Stack Developer",
-                "who loves building ",
+                "who love building ",
                 "dynamic and user-",
                 "friendly web",
                 "experiences.",
@@ -28,9 +29,23 @@ export default function Banner() {
               cursorStyle="|"
             />
           </h2>
+          <p className="font-sans">
+            Hi, I’m Jahid — a passionate MERN Stack Developer dedicated to
+            building creative, functional, and performance-driven web solutions.
+          </p>
+          <div className="flex gap-6">
+            <button className="w-[80px] border p-2 text-center items-center rounded-lg">
+              helo
+            </button>
+            <button>helo</button>
+          </div>
         </div>
-        <div className="flex-1">
-          <h2>hello 2</h2>
+        <div className="flex-1 flex items-center justify-center p-6">
+          <Lottie
+            animationData={animationData}
+            loop={true}
+            className="w-[80%] h-[80%]"
+          />
         </div>
       </div>
     </div>
